985359a1e55f5ec9228863b75835ef171494fcf8
Sourav khan <62439589+souravkhan12@users.noreply.github.com>
2024-12-30 00:38:58 +0530

    Finished App
    Added features like restart quiz and added timer using useEffect.

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useReducer } from "react";
+
 import Header from "../Header";
 import Main from "./Main";
 import Loader from "./Loader";
@@ -8,6 +9,10 @@ import Question from "./Question";
 import NextButton from "./NextButton";
 import ProgressBar from "./ProgressBar";
 import FinishScreen from "./FinishScreen";
+import Footer from "./Footer";
+import Timer from "./Timer";
+
+const SECS_PER_QUESTIONS = 30;
 
 const initialState = {
   questions: [],
@@ -16,6 +21,7 @@ const initialState = {
   index: 0,
   answer: null,
   points: 0,
+  secondsRemaining: null,
 };
 function reducer(state, action) {
   switch (action.type) {
@@ -24,7 +30,11 @@ function reducer(state, action) {
     case "dataFailed":
       return { ...state, status: "error" };
     case "start":
-      return { ...state, status: "active" };
+      return {
+        ...state,
+        status: "active",
+        secondsRemaining: state.questions.length * SECS_PER_QUESTIONS,
+      };
 
     case "newAnswer":
       const question = state.questions.at(state.index);
@@ -40,16 +50,31 @@ function reducer(state, action) {
 
     case "nextQuestion":
       return { ...state, index: state.index + 1, answer: null };
+
+    case "Restart":
+      return {
+        ...initialState,
+        questions: state.questions,
+        status: "ready",
+        secondsRemaining: 10,
+      };
+
+    case "tick":
+      return {
+        ...state,
+        secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining === 0 ? "finished" : state.status,
+      };
     default:
       throw new Error("Action unkown");
   }
 }
 
 function App() {
-  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
-    reducer,
-    initialState
-  );
+  const [
+    { questions, status, index, answer, points, secondsRemaining },
+    dispatch,
+  ] = useReducer(reducer, initialState);
   const numQuestions = questions.length;
   const totalPoints = questions.reduce((acc, curr) => acc + curr.points, 0);
 
@@ -89,11 +114,18 @@ function App() {
               dispatch={dispatch}
               answer={answer}
             />
-            <NextButton dispatch={dispatch} answer={answer} />
+            <Footer>
+              <Timer dispatch={dispatch} secondsRemaining={secondsRemaining} />
+              <NextButton dispatch={dispatch} answer={answer} />
+            </Footer>
           </>
         )}
         {status === "finished" && (
-          <FinishScreen points={points} maxPossiblePoints={totalPoints} />
+          <FinishScreen
+            points={points}
+            maxPossiblePoints={totalPoints}
+            dispatch={dispatch}
+          />
         )}
       </Main>
     </div>
--- src/components/FinishScreen.js
+++ src/components/FinishScreen.js
@@ -1,13 +1,18 @@
 import React from "react";
 
-function FinishScreen({ points, maxPossiblePoints }) {
+function FinishScreen({ points, maxPossiblePoints, dispatch }) {
   const percentage = (points / maxPossiblePoints) * 100;
 
   return (
-    <p className="result ">
-      You scored <strong>{points}</strong> out of {maxPossiblePoints} (
-      {Math.ceil(percentage)}%)
-    </p>
+    <>
+      <p className="result ">
+        You scored <strong>{points}</strong> out of {maxPossiblePoints} (
+        {Math.ceil(percentage)}%)
+      </p>
+      <div className="btn btn-ui" onClick={() => dispatch({ type: "Restart" })}>
+        Restart
+      </div>
+    </>
   );
 }
 
